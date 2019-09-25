3202cb97d4794fb8328db96c4e0a97a536a78abc
Ilman Nafian <55781619+ilmannafian04@users.noreply.github.com>
2019-09-25 15:42:13 +0700

    Finter (#5)
    * creating program0 for cl4b
    
    * problem 2 and 3
    
    * working on checklist04b
    
    * delete unnecessary file
    
    * added sharedmem init

--- SandBox/01-Finter/daystram/cl4b/program0.c
+++ SandBox/01-Finter/daystram/cl4b/program0.c
@@ -3,24 +3,53 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/mman.h>
 
+#define BUFFER_SIZE 32
+#define MYFLAGS      O_RDWR
+#define MYPROTECTION PROT_READ|PROT_WRITE
+#define MYVISIBILITY MAP_SHARED
+
+char*  sfile="demo-file.bin";
+
+typedef struct {
+    char data[BUFFER_SIZE];
+} response;
 
 int main(int argc, char *argv[]) {
-    char queue[16][32];
+    char line[BUFFER_SIZE];
     FILE *fp = fopen("queue.txt", "r");
-    int i = 0;
-    char *response; // variable to be shared
+    
+    struct stat fsize;
+    int ssize = sizeof(response);
+    int fd = open(sfile,MYFLAGS,S_IRWXU);
 
-    while(fgets(queue[i], sizeof queue, fp) != NULL) {
-        queue[i][strlen(queue[i]) - 1] = '\0';
+    printf("Create mmap()\n");
+    fstat(fd, &fsize);
+    if (fsize.st_size < ssize) {
+        printf("Does %s exist? Size >= %d?\n", sfile, ssize);
+        exit(1);
+    }
+    response* mymap=mmap(NULL, ssize, MYPROTECTION, MYVISIBILITY, fd, 0);
+    if (mymap == MAP_FAILED) {
+        printf("mmap(): FAILED\n");
+        exit(1);
+    }
+    mymap->mystring[0]='X';
+
+
+
+    while(fgets(line, BUFFER_SIZE, fp) != NULL) {
+        line[strlen(line) - 1] = '\0';
         if (!fork()) {
             char executable[34];
-            sprintf(executable, "./%s", queue[count-1]);
-            execlp(executable, executable, NULL);
+            //sprintf(executable, "./%s", queue[count-1]);
+            //execlp(executable, executable, NULL);
+            printf("%s\n", line);
         } else {
             wait(NULL);
             printf("%s\n", response);
-        i++;
+        }
     }
     fclose(fp);
 }
