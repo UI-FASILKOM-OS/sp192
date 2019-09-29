eff6b4881f8422112e420cd5f1a7e78f9aecbc8a
Danny August Ramaputra <daystram@gmail.com>
2019-09-29 12:53:48 +0000

    completed checklist4b

--- SandBox/01-Finter/daystram/cl4b/program2.c
+++ SandBox/01-Finter/daystram/cl4b/program2.c
@@ -14,6 +14,8 @@
 #define VISIBILITY  MAP_SHARED
 #define SFILE       "demo-file.bin"
 
+#define RESPONSE    "program2"
+
 typedef struct {
     sem_t sync;
     char  data[BUFFER_SIZE];
@@ -22,32 +24,15 @@ typedef struct {
 int main(int argc, char *argv[]) {
     int mem = open(SFILE, FLAGS, S_IRWXU);
     int ssize = sizeof(response);
-    truncate(SFILE, ssize);
+
     response* mymap = mmap(NULL, ssize, PROTECTION, VISIBILITY, mem, 0);
     if (mymap == MAP_FAILED) {
         printf("mmap(): FAILED\n");
         exit(1);
     }
 
-    sem_init(&(mymap->sync), 1, 0);
+    memcpy(mymap->data, RESPONSE, sizeof(RESPONSE));
+    sem_post(&(mymap->sync));
 
-    char line[BUFFER_SIZE];
-    FILE* queue = fopen("queue.txt", "r");;
-    while(fgets(line, BUFFER_SIZE, queue) != NULL) {;
-        line[strlen(line) - 1] = '\0';
-        if (access(line, F_OK|X_OK) == 0) {
-            if (!fork()) {
-                char executable[BUFFER_SIZE+2];
-                sprintf(executable, "./%s", line);
-                execlp(executable, executable, NULL);
-            } else {
-                wait(NULL);
-                sem_wait(&(mymap->sync));
-                printf("%s\n", mymap->data);
-            }
-        }
-    }
-
-    fclose(queue);
     close(mem);
 }
