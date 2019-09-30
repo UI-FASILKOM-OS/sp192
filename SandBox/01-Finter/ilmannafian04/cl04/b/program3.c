44bd24225d4524cb021b35df455c768d8c33b26c
Ilman Nafian <55781619+ilmannafian04@users.noreply.github.com>
2019-09-30 12:57:32 +0700

    Finter (#8)
    * creating program0 for cl4b
    
    * problem 2 and 3
    
    * working on checklist04b
    
    * delete unnecessary file
    
    * added sharedmem init
    
    * creating for week04b
    
    * completed checklist4b
    
    * finish checklist 4b

--- SandBox/01-Finter/ilmannafian04/cl04/b/program3.c
+++ SandBox/01-Finter/ilmannafian04/cl04/b/program3.c
@@ -3,29 +3,32 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 	32
+#define FLAGS		O_CREAT | O_RDWR
+#define PROTECTION	PROT_READ | PROT_WRITE
+#define VISIBILITY	MAP_SHARED
+#define SFILE		"demo-file.bin"
+#define RESPONSE	"program3"
+
+typedef struct {
+	sem_t sync;
+	char data[BUFFER_SIZE];
+} response;
 
 void main(int argc, char *argv[]) {
-	int count = 0;
-	char queue [16][32];
-	FILE *fp = fopen("queue.txt", "r");
-	int i = 0;
-	while(fgets(queue[i], sizeof queue, fp) != NULL) {
-		queue[i][strlen(queue[i]) - 1] = '\0';
-		if (strcmp("program3", queue[i]) == 0) {
-			count = i;
-		}
-		i++;
+	int mem = open(SFILE, FLAGS, S_IRWXU);
+	int ssize = sizeof(response);
+	response* mymap = mmap(NULL, ssize, PROTECTION, VISIBILITY, mem, 0);
+	if (mymap == MAP_FAILED) {
+		printf("error\n");
+		exit(1);
 	}
-
-	fclose(fp);
-	if (!fork()) {
-		if (count > 0) {
-			char executable[34];
-			sprintf(executable, "./%s", queue[count - 1]);
-			execlp(executable, queue[count - 1], NULL);
-		}
-	} else {
-		wait(NULL);
-		printf("Hello, program3\n");
-	}
+	memcpy(mymap->data, RESPONSE, sizeof(RESPONSE));
+	sem_post(&(mymap->sync));
+	close(mem);
 }
