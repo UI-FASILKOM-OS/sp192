ff8dc386ac3843fe6106e7caf94dbdb1732b78fe
Rizal <rizal.alfaridzi@gmail.com>
2019-10-02 08:36:41 +0000

    scripta.sh add agaian

--- SandBox/03-Soiberry/shared05/scripta.sh
+++ SandBox/03-Soiberry/shared05/scripta.sh
@@ -1,74 +1,51 @@
+#!/bin/bash
+# REV11: Mon Aug 27 19:18:47 WIB 2018
+# START: Mon Sep  5 11:12:35 WIB 2016
+
+# R: These are collections of command lines. Run "bash a03-command-lines-demo 11 22 33 44 55"
+
+# Copyright (C) 2016-2018 Rahmat M. Samik-Ibrahim
+# http://RahmatM.Samik-Ibrahim.vLSM.org/
+# This program is free script/software. This program is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 XX="xx"
-HEAD="a"
+TMPFILE="ZA-thisfile.tx"
+TESTDIR1="ZB-SOURCE"
+TESTDIR2="ZC-BACKUP"
+TEST_FILE="test-file.txt"
 
-PREFIX="^#.R:"
-RESULT=`grep $PREFIX $0`
-clear
-if [[ $RESULT ]] ; then
-   printf "\n[%11s]: %s\n" "`cut -c 1-11 <<< $0`" "$RESULT"
-fi
-echo ""; 
-echo "This screen size should be at least \"80 x 23\" characters..."
-echo "RESIZE the screen if this following message does not fit in \"80 x 23\""
-echo ""; echo "*** HIT ENTER KEY ***";
+[ -d $TESTDIR1 ] && [ -d $TESTDIR2 ] && [ -f $TMPFILE ] || {
+   echo ""; echo "No $TESTDIR1 or $TESTDIR2 or $TMPFILE."
+   echo "Please run script \"a02-sort-n-prepare\" first!"; echo ""
+   exit
+}
+
+echo ""; echo "These are collections of command lines."
+echo "Just run: \"bash $0 11 22 33 44 55\""
+echo ""; echo "*** Hit Enter Key ***";
 [ "$1" = "$XX" ] || (read YY)
 
-cat - << NNNN
-01 START START START START START START START START START START START START START
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-         10        20        30        40        50        60        70       79
-
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-
-         10        20        30        40        50        60        70       79
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-09 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-XXXXX                           TEST FOR 80 x 23                           XXXXX
-
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-         10        20        30        40        50        60        70       79
-
-         10        20        30        40        50        60        70       79
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-19 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-         10        20        30        40        50        60        70       79
-12345678911234567892123456789312345678941234567895123456789612345678971234567898
-23 END END END END END END END END END  ====   H I T   E N T E R   K E Y   =====
-NNNN
-
-[ "$1" = "$XX" ] || (read YY)
-echo ""; echo "Directory listing (running \"ls -AF\"):"
-echo "================================================================================"
-ls -AF
-echo "================================================================================"
-echo ""; echo "*** HIT ENTER KEY ***";
-[ "$1" = "$XX" ] || (read YY)
-
-cat - << NNNN
-================================================================================
-TAKE NOTE: 
-
-This is JUST a "READ ME" file for this demo.
-If you do not understand, just IGNORE THIS SCRIPT!
-
-Rahmat M. Samik-Ibrahim
-================================================================================
-NNNN
-
-echo ""; echo "*** HIT ENTER KEY ***";
-[ "$1" = "$XX" ] || (read YY)
-
-echo ""; echo "[-----------]: # R: LISTING THE LINES ========  ==  ==  ==  ==  ==  ==  ========"
-for II in ${HEAD}* ; do
-   RESULT=`grep $PREFIX $II`
-   if [[ $RESULT ]] ; then
-      printf "[%11s]: %s\n" "`cut -c 1-11 <<< $II`" "$RESULT"
-   fi
+cat $TEST_FILE | while read II; do
+   echo $II
+    COMMENT="RUNNING:  $II"
+    SIZE=${#COMMENT}
+    (( $SIZE > 80 )) && SIZE=80
+    STR=$(eval printf "%0.s=" {1..$SIZE})
+    echo "$STR"
+    echo $COMMENT
+    echo "$STR"
+    eval $II
+    echo ""; echo "*** Hit Enter Key ***";
+    [ "$1" = "$XX" ] || (read YY)
 done
 
-exit
 
+echo ""; 
+echo "This screen size should be at least \"80 x 23\" characters..."
+echo "RESIZE the screen if this following message does not fit in \"80 x 23\"";
+echo "";
+
+
+exit
