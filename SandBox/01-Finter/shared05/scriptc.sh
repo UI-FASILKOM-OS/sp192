5d4569145dec42d2c2f27c6bfdadb0b0bb78771b
wahyuadt <wahyuadt9@gmail.com>
2019-10-07 07:44:45 +0000

    Test-file

--- SandBox/01-Finter/shared05/scriptc.sh
+++ SandBox/01-Finter/shared05/scriptc.sh
@@ -21,7 +21,50 @@ TEST_FILE="test-file.txt"
    echo "Please run script \"a02-sort-n-prepare\" first!"; echo ""
    exit
 }
+echo "";
+echo "This screen size should be at least \"80 x 23\" characters..."
+echo "RESIZE the screen if this following message does not fit in \"80 x 23\""
+echo ""; echo "*** HIT ENTER KEY ***";
+[ "$1" = "$XX" ] || (read YY)
 
+cat - << NNNN
+01 START START START START START START START START START START START START START
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+         10        20        30        40        50        60        70       79
+
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+
+         10        20        30        40        50        60        70       79
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+09 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+XXXXX                           TEST FOR 80 x 23                           XXXXX
+
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+         10        20        30        40        50        60        70       79
+
+         10        20        30        40        50        60        70       79
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+19 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+         10        20        30        40        50        60        70       79
+12345678911234567892123456789312345678941234567895123456789612345678971234567898
+23 END END END END END END END END END  ====   H I T   E N T E R   K E Y   =====
+NNNN
+
+
+[ "$1" = "$XX" ] || (read YY)
+
+
+mkdir -p $TESTDIR1 $TESTDIR2
+touch "$TESTDIR1/abcd 'xyz NNNN"
+touch "$TESTDIR1/#wah , berkas * ini ^ aneh & ajaib"
+touch "$TESTDIR1/[email anda : cicak@bin.kadal?!]"
+touch "$TESTDIR1/x y z \" z y x"
+touch "$TESTDIR1/x y z \" (z y) x"
+touch "$TESTDIR1/x y z \" (z y)"
+chmod -R 755 $TESTDIR1
 echo ""; echo "These are collections of command lines."
 echo "Just run: \"bash $0 11 22 33 44 55\""
 echo ""; echo "*** Hit Enter Key ***";
