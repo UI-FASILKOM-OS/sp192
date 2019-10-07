12206caa04c0eabb25ef7071a6e1fdd6cbf4a9b0
anggrafazza <nugierianto04@gmail.com>
2019-10-07 06:52:08 +0000

    simpleread?

--- SandBox/02-KukFight/anggrafazza/week06/exercise1.sh
+++ SandBox/02-KukFight/anggrafazza/week06/exercise1.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-head -n 14105 < apache.txt | tail -n +335 > 
-tmp.txt
-(())
-awk 'BEGIN{
-total = 0;
-} 
-{
-sub(/^[^"]*"[^"]*" [^ ]* /,""); 
-total += $1;
-} 
-END {
-printf("%d\n", total);}' tmp.txt > total.txt
+#!/bin/basah
 
+head -n 14105 < apache.txt | tail -n +335 > tmp.txt
 
+awk 'BEGIN {
+        total = 0;
+    } {
+        sub(/^[^"]*:[^:]*" [^ ]* /,"");
+        total += $1;
+    } END {
+        printf("%d\n", total);
+    }' tmp.txt > total.txt
